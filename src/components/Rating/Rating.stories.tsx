e6f7bad61002ebc0c27a0b400cdaf87860c2a07c
Tont <yamalauto1@yandex.ru>
2021-08-08 21:47:44 +0500

    PC Home 10 lesson (16 min)1

---
+++ src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Rating} from "./Rating";
+
+export default {
+  title: 'Rating stories',
+  component: Rating,
+}
+
+export const EmptyRating = () => <Rating value={0} onClick={x => x}/>
+export const Rating1 = () => <Rating value={1} onClick={x => x}/>
+export const Rating2 = () => <Rating value={2} onClick={x => x}/>
+export const Rating3 = () => <Rating value={3} onClick={x => x}/>
+export const Rating4 = () => <Rating value={4} onClick={x => x}/>
+export const Rating5 = () => <Rating value={5} onClick={x => x}/>
