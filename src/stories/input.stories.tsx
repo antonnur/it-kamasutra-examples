ecf8700716d4622c17fcf6f7fb99f072c383f406
Tont <yamalauto1@yandex.ru>
2021-08-21 21:40:41 +0500

    PC Home 13 lesson (End)

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState} from "react";
+import {action} from "@storybook/addon-actions";
 
 export default {
   title: 'input',
@@ -30,6 +31,32 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
   return <><input ref={inputRef}/>
     <button onClick={save}>save</button>
     - actual value: {value} </>
+}
+
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState('')
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {setParentValue(e.currentTarget.value)}
+  return <input value={parentValue}  onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(true)
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {setParentValue(e.currentTarget.checked)}
+  return <input type={'checkbox'} checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>('2')
+
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {setParentValue(e.currentTarget.value)}
+  return <select value={parentValue} onChange={onChange}>
+    <option>none</option>
+    <option value={"1"}>Sib</option>
+    <option value={"2"}>Omsk</option>
+    <option value={"3"}>Tum</option>
+  </select>
 }
 
 export const ControlledInputWithFixedValue = () => <input value={'it-incubator'}/>
