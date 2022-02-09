299ef2c0e2b3e05c1d01e112609411022ba7f765
Tont <yamalauto1@yandex.ru>
2022-02-09 21:46:27 +0500

    PC Home 21 lesson (End) 22 - useEffect - setTimeout, setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import {cleanup} from "@testing-library/react";
 
 export default {
   title: 'useEffect demo',
@@ -10,7 +11,7 @@ export const SimpleExample = () => {
   const [fake, setFake] = useState(1)
   const [counter, setCounter] = useState(1) // [ 0, function(newValue){} ]
 
-  console.log('SimpleExample')
+  console.log('SimpleExample !')
 
   useEffect(() => {
     console.log("useEffect every render")
@@ -29,9 +30,82 @@ export const SimpleExample = () => {
   }, [counter])
 
   return <>
-    Hello, {counter} {fake}
+    Hello, Counter: {counter} - Fake: {fake}
     <button onClick={() => setFake(fake + 1)}>Fake+</button>
     <button onClick={() => setCounter(counter + 1)}>Counter+</button>
+  </>
+}
+
+// 22.  useEffect \ setTimeout
+
+export const SetTimeoutExample = () => {
+  const [fake, setFake] = useState(1)
+  const [counter, setCounter] = useState(1) // [ 0, function(newValue){} ]
+
+  console.log('SetTimeoutExample')
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      console.log("setTimeout !")
+      document.title = counter.toString()
+    }, 1000)
+
+  }, [counter])
+
+  return <>
+    Counter: {counter} - Fake: {fake}
+    <button onClick={() => setFake(fake + 1)}>Fake+</button>
+    <button onClick={() => setCounter(counter + 1)}>Counter+</button>
+  </>
+}
+
+// 22.  useEffect \ setInterval
+
+export const SetIntervalExample = () => {
+  const [counter, setCounter] = useState(1) // [ 0, function(newValue){} ]
+
+  console.log('SetIntervalExample')
+
+  useEffect(() => {
+
+    console.log('useEffect !')
+
+    setInterval(() => {
+      console.log("tick: " + counter)
+      setCounter((state) => state + 1)
+    }, 1000)
+
+  }, [])
+
+  return <>
+    Counter: {counter}
+  </>
+}
+
+// 22.  useEffect \ сделать часы
+
+export const Clock = () => {
+  const [date, setDate] = useState(new Date())
+
+  const refreshClock = () => {
+    setDate(new Date())
+  }
+
+  // console.log(date)
+
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000)
+
+     return function cleanup() { //Memory leak
+       clearInterval(timerId)
+     }
+  }, [])
+
+  return <>
+    <h3>
+      {date.toLocaleTimeString()}
+    </h3>
   </>
 }
 
